6c8cdf4438d6ae9357b8d29443d5a856f21125a3
jsherard <jeff.sherard@gmail.com>
2013-07-30 21:28:07 -0600

    Completed Ch9_Ex2 homework

--- C++/Chapter 9/Chp09_Ex02/Chp09_Ex02/Chp09_Ex02.cpp
+++ C++/Chapter 9/Chp09_Ex02/Chp09_Ex02/Chp09_Ex02.cpp
@@ -3,46 +3,69 @@
 // Ch09_Ex02 Program
 
 #include <iostream>
-#include<fstream>
+#include <fstream>
 #include <iomanip>
 #include <string>
 
 using namespace std;
 
-// Constant
+// Constant - to set class size
 
-const int NUM_STUDENTS = 10;
+const int NUM_STUDENTS = 20;
 
-// Structs
+// Structs - create student info (to store in an array)
 struct studentType
 {
 	string studentFName;
 	string studentLName;
 	int testScore;
-	char grade;
+	char grade;	
 };
 
-// Functon Prototypes
-void inputData();
-void assignGrade();
-void highestGrade();
-void outputData();
+// Functon Prototypes  **CURRENTLY SET FOR VOID - May pass data later**
+void inputData(ifstream& infile, studentType list[], int listSize);
+void assignGrade(studentType list[], int listSize);
+int highestScore(studentType list[], int listSize);
+void outputData(ofstream& outfile, studentType list[], int listSize, int temp);
 
 int main()
 {
+	// Open Input/Output File - Ch9_Ex2Data.txt (studentFName studentLlName score) + Ch9_ExOut.txt
+	ifstream infile;
+	ofstream outfile;
+	string inputFile = "Ch9_Ex2Data.txt";
+	string outputFile = "Ch9_Ex2out.txt";
+
 	// Declare Variables and Arrays
+	int temp;
 	studentType classStudents[NUM_STUDENTS];
+
+	infile.open(inputFile.c_str());
+
+	// if data file doesn't exist error out
+	if (!infile)
+	{
+		cout << "Cannot open the input file." << endl;
+	}
 	
 	// Input
-	inputData();
+	inputData(infile, classStudents, NUM_STUDENTS);
+
+	infile.close();
+	infile.clear();
 
 	// Calculate
-	assignGrade();
+	assignGrade(classStudents, NUM_STUDENTS);
+	temp = highestScore(classStudents, NUM_STUDENTS);
 
-	highestGrade();
+	// put data in output file
+	outfile.open(outputFile.c_str());
 
 	//Output
-	outputData();
+	outputData(outfile, classStudents, NUM_STUDENTS, temp);
+
+	// close output file
+	outfile.close();
 
 	cout << endl;
 
@@ -51,50 +74,95 @@ int main()
 	return 0;
 }
 
-void inputData()
+void inputData(ifstream& indata, studentType list[], int listSize)
 {
-	ifstream infile;
-	string inputFile;
+	int index;
 
-	cout << "Enter the input file name: ";
-	cin >> inputFile;
-	cout << endl;
-
-	infile.open(inputFile.c_str());
-
-	if (!infile)
+	// put studentType data into classStudents array
+	for (index = 0; index < listSize; index++)
 	{
-		cout << "Cannot open the input file." << endl;
+		indata  >> list[index].studentFName
+				>> list[index].studentLName
+				>> list[index].testScore;
+		list[index].grade = ' ';
+	}
+}
+
+void assignGrade(studentType list[], int listSize)
+{
+// Grading scale
+/*	90 - 100 = A
+	80 - 89 = B
+	70 - 79 = C
+	60 - 69 = D
+	0 - 59 = F
+*/
+	int index;
+
+	for (index = 0; index < listSize; index++)
+		if (list[index].testScore >= 90)
+			list[index].grade = 'A';
+		else if (list[index].testScore >= 80)
+			list[index].grade = 'B';
+		else if (list[index].testScore >= 70)
+			list[index].grade = 'C';
+		else if (list[index].testScore >= 60)
+			list[index].grade = 'D';
+		else list[index].grade = 'F';
+}
+
+int highestScore(studentType list[], int listSize)
+{
+	int index;
+	int smallestIndex;
+	int location;
+	int temp;
+	int tempArray[NUM_STUDENTS];
+
+	// put test scores in a temporary array to sort
+	for (index = 0; index < listSize; index++)
+		tempArray[index] = list[index].testScore;
+
+
+	// do an array selection sort to find the highest score
+	for (index = 0; index < listSize - 1; index++)
+	{
+		smallestIndex = index;
+
+		for (location = index + 1; location < listSize; location++)
+			if (tempArray[location] < tempArray[smallestIndex])
+				smallestIndex = location;
+
+		temp = tempArray[smallestIndex];
+		tempArray[smallestIndex] = tempArray[index];
+		tempArray[index] = temp;
 	}
 
-	infile.close();
-	infile.clear();
+	temp = tempArray[19];
+
+	return temp;
 }
 
-void assignGrade()
+void outputData(ofstream& outfile, studentType list[], int listSize, int temp)
 {
+	//Print to file Names Scores Grades
+	outfile << "Student Name" <<setw(21) << "Test Score" << setw(7) << "Grade" << endl;
 
-}
+	for (int index = 0; index < NUM_STUDENTS; index++)
+	{
+		string tempName = list[index].studentLName + ", " + list[index].studentFName;
+		outfile << left << setw(29) << tempName;
+		outfile << setw(8) << list[index].testScore << setw(15) << list[index].grade << endl;
+	}
 
-void highestGrade()
-{
+	outfile << endl;
+	outfile << "Highest Test Score: " << temp << endl;
+	outfile << "Students having the highest test score:" << endl;
 
-}
-
-void outputData()
-{
-	ofstream outfile;
-	string outputFile;
-
-	cout << "Enter the output file name: ";
-	cin >> outputFile;
-	cout << endl;
-
-	outfile.open(outputFile.c_str());
-
-	outfile.close();
-
-	cout << left << setw(25) << "Student Name" << setw(12) << "Test Score" << setw(10) << "Grade" << endl;
+	// Print to file student names of those with the highest test score
+	for (int index = 0; index < NUM_STUDENTS; index++)
+		if (list[index].testScore == temp)
+			outfile << list[index].studentLName << ",  " << list[index].studentFName << endl;
 
 	cout << "Please see the results in Ch9_Ex2Out.txt";
 }
--- C++/template/template/homework.cpp
+++ C++/template/template/homework.cpp
@@ -1,20 +1,44 @@
 // Programmer:  Jeff Sherard
-// Test Program
+// Program to find the average test scores and output the average
+// test score and all the test scores that are less than
+// the average test score.
+
+// 85 62 94 56 71
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
 
-char str[15];
+int test[5];
+int sum = 0;
+double average;
+int index;
 
-strcpy(str, "Blue Sky");
+cout << fixed << showpoint << setprecision(2);
 
-cout << "str =" << str << endl;
+cout << "Enter five test scores: ";
 
-	system ("pause");
+for (index = 0; index < 5; index++)
+{
+	cin >> test[index];
+	sum = sum + test[index];
+}
+
+average =  sum / 5.0;
+
+cout << "The average test score is: " << average << endl;
+
+for (index = 0; index < 5; index++)
+	if (test[index] < average)
+		cout << test[index] << " is less than the average test score." << endl;
+
+cout << endl;
+
+system ("pause");
 		
-	return 0;
+return 0;
 }
